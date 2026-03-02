1078428350deaaf0d3b4238a3c7092eb7cea5449
Alexandre Queiroz <1619912+arcanjoaq@users.noreply.github.com>
2026-03-02 18:31:30 -0300

    fixes #1 (#2)

--- src/clojure_clj_http_examples/04_mtls.clj
+++ src/clojure_clj_http_examples/04_mtls.clj
@@ -1,4 +1,4 @@
-(ns clojure-clj-http-examples.03-trust-store
+(ns clojure-clj-http-examples.04-mtls
   (:require
    [clj-http.client :as client]
    [schema.core :as s])
@@ -7,10 +7,15 @@
 
 (s/defn https-get-request
   [url         :- s/Str
+   key-store   :- KeyStore
    trust-store :- KeyStore]
   (client/get url {:socket-timeout 1000
                    :connection-timeout 1000
+                   :keystore           key-store
+                   :keystore-pass      "changeit"
+                   :keystore-type      "PKCS12"
                    :trust-store        trust-store
                    :trust-store-type   "jks"
                    :trust-store-pass   ""
                    :insecure?          false}))
+
--- test/clojure_clj_http_examples/04_mtls_test.clj
+++ test/clojure_clj_http_examples/04_mtls_test.clj
@@ -1,7 +1,7 @@
-(ns clojure-clj-http-examples.03-trust-store-test
+(ns clojure-clj-http-examples.04-mtls-test
   (:require
    [clj-test-containers.core :as tc]
-   [clojure-clj-http-examples.03-trust-store :refer [https-get-request]]
+   [clojure-clj-http-examples.04-mtls :refer [https-get-request]]
    [clojure-clj-http-examples.certs :as certs]
    [clojure-clj-http-examples.keystores :as keystores]
    [schema.core :as s]
@@ -16,7 +16,7 @@
                   :wait-for {:http {:path "/health"
                                     :port 80
                                     :status 200}}})
-      (tc/bind-filesystem! {:host-path "resources/nginx_tls.conf"
+      (tc/bind-filesystem! {:host-path "resources/nginx_mtls.conf"
                             :container-path "/etc/nginx/nginx.conf"
                             :mode :read-only})
 
@@ -27,22 +27,30 @@
       (tc/bind-filesystem! {:host-path "/tmp/certificate.pem"
                             :container-path "/tmp/certificate.pem"
                             :mode :read-only})
+
+      (tc/bind-filesystem! {:host-path "/tmp/root-ca.pem"
+                            :container-path "/tmp/root-ca.pem"
+                            :mode :read-only})
+
       (tc/start!)))
 
 (defn- create-certs!
   []
   (let [root (certs/self-signed-root-ca "C=BR, ST=Sao Paulo, L=Sao Paulo, O=My Root CA, OU=IT, CN=api.*" 365)
-        server (certs/server-cert root "C=BR, ST=Sao Paulo, L=Sao Paulo, O=My Org, OU=IT, CN=api.localhost" 825)]
+        server-cert (certs/server-cert root "C=BR, ST=Sao Paulo, L=Sao Paulo, O=My Org, OU=IT, CN=api.localhost" 825)
+        client-cert (certs/client-cert root "C=BR, ST=Sao Paulo, L=Sao Paulo, O=My Org, OU=IT, CN=mTLS" 825)
+        keystore (certs/create-keystore (:keypair client-cert) (:certificate client-cert) "changeit")]
     (certs/save-key-to-der (:keypair root) "/tmp/root-ca-key.der")
     (certs/save-cert-to-der (:certificate root) "/tmp/root-ca.der")
     (certs/save-key-to-pem (:keypair root) "/tmp/root-ca-key.pem")
     (certs/save-cert-to-pem (:certificate root) "/tmp/root-ca.pem")
-    (certs/save-key-to-der (:keypair server) "/tmp/key.der")
-    (certs/save-cert-to-der (:certificate server) "/tmp/certificate.der")
-    (certs/save-key-to-pem (:keypair server) "/tmp/key.pem")
-    (certs/save-cert-to-pem (:certificate server) "/tmp/certificate.pem")))
+    (certs/save-key-to-der (:keypair server-cert) "/tmp/key.der")
+    (certs/save-cert-to-der (:certificate server-cert) "/tmp/certificate.der")
+    (certs/save-key-to-pem (:keypair server-cert) "/tmp/key.pem")
+    (certs/save-cert-to-pem (:certificate server-cert) "/tmp/certificate.pem")
+    (certs/save-keystore keystore "/tmp/keystore.jks" "changeit")))
 
-(defflow trust-store-test
+(defflow mtls-test
   (flow "Creating certificates"
         (flow/invoke create-certs!)
 
@@ -53,7 +61,8 @@
                     ;; add 127.0.0.1 api.localhost in your /etc/hosts
                     [:let [base-url (str "https://api.localhost:" (get (:mapped-ports container) 443))
                            res (https-get-request (str base-url "/health")
-                                                  (keystores/->keystore "/tmp/certificate.pem"))]]
+                                                  (keystores/pkcs12->keystore "/tmp/keystore.jks" "changeit")
+                                                  (keystores/->keystore "/tmp/root-ca.pem"))]]
                     (flow "Then the response will be 200"
                           (match? {:status 200} res)))
 
@@ -61,7 +70,8 @@
                     ;; add 127.0.0.1 api.localhost in your /etc/hosts
                     [:let [base-url (str "https://api.localhost:" (get (:mapped-ports container) 443))
                            res (https-get-request (str base-url "/health")
-                                                  (keystores/->keystore "/tmp/certificate.der"))]]
+                                                  (keystores/pkcs12->keystore "/tmp/keystore.jks" "changeit")
+                                                  (keystores/->keystore "/tmp/root-ca.der"))]]
                     (flow "Then the response will be 200"
                           (match? {:status 200} res)))
 
@@ -76,14 +86,14 @@
 
   (swap! container tc/stop!)
 
-  (->
-   (https-get-request
-    (str "https://api.localhost:" (get (:mapped-ports @container) 443) "/health")
-    (keystores/->keystore "/tmp/certificate.pem"))
-   :body)
+  (-> (https-get-request
+       (str "https://api.localhost:" (get (:mapped-ports @container) 443) "/health")
+       (keystores/pkcs12->keystore "/tmp/keystore.jks" "changeit")
+       (keystores/->keystore "/tmp/certificate.pem"))
+      :body)
 
-  (->
-   (https-get-request
-    (str "https://api.localhost:" (get (:mapped-ports @container) 443) "/health")
-    (keystores/->keystore "/tmp/certificate.der"))
-   :body))
+  (-> (https-get-request
+       (str "https://api.localhost:" (get (:mapped-ports @container) 443) "/health")
+       (keystores/pkcs12->keystore "/tmp/keystore.jks" "changeit")
+       (keystores/->keystore "/tmp/certificate.der"))
+      :body))
